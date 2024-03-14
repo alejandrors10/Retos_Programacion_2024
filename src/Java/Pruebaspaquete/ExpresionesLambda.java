b1f2df57ce94bfe33136cc933355d8ddf5fe954d
alejandroruiz10 <123484881+nutriamarciana@users.noreply.github.com>
2024-03-14 18:17:35 +0100

    Pruebas con File

---
+++ src/Java/Pruebaspaquete/ExpresionesLambda.java
@@ -0,0 +1,19 @@
+package Java.Pruebaspaquete;
+
+public class ExpresionesLambda {
+    public static void main(String[] args) {
+        StringFunction exclaim = (s) -> s + "!";
+        StringFunction ask = (s) -> s + "?";
+        printFormatted("Hello", exclaim);
+        printFormatted("Hello", ask);
+    }
+    public static void printFormatted(String str, StringFunction format) {
+        String result = format.run(str);
+        System.out.println(result);
+    }
+}
+interface StringFunction {
+    String run(String str);
+}
+
+
